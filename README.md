c189df3fb78c4c328a0de42f8ee690b4c9cb69ff
Igors Krasjukovs <tahq69@gmail.com>
2017-11-21 17:46:05 +0200

    README.md updated and code snippet from site added;

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # azure-storage
 Learning of azure blob storage communication from client UI
+
+## references
+
+- Repository: https://github.com/tahq69/azure-storage.git
+- Dev URL: http://localhost:8080/
+- Portal: https://portal.azure.com
+
+## TODO:
+- Tutorial: https://rawgit.com/Azure/azure-storage-node/master/browser/samples/sample-blob.html
+- Other tutorials: https://github.com/Azure/azure-storage-node/tree/master/browser/samples
--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import settings from "./settings"
 
 var blobUri = `https://${settings.STORAGE_ACCOUNT}.blob.core.windows.net`
 var blobService = AzureStorage.createBlobServiceWithSas(blobUri, settings.SAS_TOKEN)
+    .withFilter(new AzureStorage.ExponentialRetryPolicyFilter())
 
-blobService.listContainersSegmented(null, function (error, results) {
+
+blobService.createBlockBlobFromText('uploads', 'myblob', 'text.value', function (error, result, response) {
+    if (error) {
+        alert('Upload filed, open browser console for more detailed info.')
+        console.log(error)
+    } else {
+        alert('Upload successfully!')
+    }
+});
+
+
+/*blobService.listContainersSegmented(null, function (error, results) {
     if (error) {
         // List container error
         console.log(1, "error", error)
@@ -13,6 +25,6 @@ blobService.listContainersSegmented(null, function (error, results) {
             console.log(2, "container", container)
         }
     }
-})
+})*/
 
 console.log(1)
